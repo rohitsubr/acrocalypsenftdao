b5087b215e9dc2e331b87431afa1022f2c76fd52
rohitsubr <rohitsubr@gmail.com>
2022-04-24 22:49:35 +0530

    first commit

--- smart-contract/config/CollectionConfig.ts
+++ smart-contract/config/CollectionConfig.ts
@@ -9,25 +9,25 @@ const CollectionConfig: CollectionConfigInterface = {
   // The contract name can be updated using the following command:
   // yarn rename-contract NEW_CONTRACT_NAME
   // Please DO NOT change it manually!
-  contractName: 'YourNftToken',
-  tokenName: 'My NFT Token',
-  tokenSymbol: 'MNT',
+  contractName: 'WhizcatCapital',
+  tokenName: 'whiz',
+  tokenSymbol: 'WCAP',
   hiddenMetadataUri: 'ipfs://__CID__/hidden.json',
-  maxSupply: 10000,
+  maxSupply: 50,
   whitelistSale: {
-    price: 0.05,
+    price: 0.0005,
     maxMintAmountPerTx: 1,
   },
   preSale: {
-    price: 0.07,
+    price: 0.0007,
     maxMintAmountPerTx: 2,
   },
   publicSale: {
-    price: 0.09,
+    price: 0.00009,
     maxMintAmountPerTx: 5,
   },
-  contractAddress: null,
-  marketplaceIdentifier: 'my-nft-token',
+  contractAddress: "0xe373F296fF8738F4862C7aE0ebc1bac595C14FEC",
+  marketplaceIdentifier: 'whizcatcapital',
   marketplaceConfig: Marketplaces.openSea,
   whitelistAddresses,
 };
